05116be8b06db6e1781589bd5e81f3f616de9eb4
BahaniBillel <billel.bahani@gmail.com>
2023-01-14 10:07:20 +0100

    improving navigation bar by adding sublinks,and building websites service page

--- pages/services/index.js
+++ pages/services/index.js
@@ -5,6 +5,18 @@ import Navigation from "../../components/navigation";
 
 function Services() {
   const router = useRouter();
+  const pagelinks = [
+    { id: 1, name: "eCommerce website", url: "#" },
+    { id: 2, name: "Business website", url: "#" },
+    { id: 3, name: "Blog website", url: "#" },
+    { id: 4, name: "Portfolio website", url: "#" },
+    { id: 5, name: "Event website", url: "#" },
+    { id: 6, name: "Personal website", url: "#" },
+    { id: 7, name: "Membership website", url: "#" },
+    { id: 8, name: "Nonprofit website", url: "#" },
+    { id: 9, name: "Informational website", url: "#" },
+    { id: 10, name: "Online forum", url: "#" },
+  ];
 
   const { sid } = router;
   return (
